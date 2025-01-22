b94609f3a128a2cee7a59709588b00b2ec88cb60
Titoputraaa <titobamulo13@gmail.com>
2025-01-22 07:22:45 +0700

    dectement method

--- Rekursif.java
+++ Rekursif.java
@@ -1,1 +1,19 @@
-
+public class Rekursif {
+    // Program mencetak angka n sampai 1 (decrement)
+    // Program mencetak angka 1 sampai n (increment)
+    // Faktorial
+    // Pangkat
+    // Fibonacci
+    
+        public static void main(String[] args) {
+            // decrement(5);
+        }
+    
+        static void decrement(int n) {
+            if (n <= 0) {
+                return;
+            }
+            System.out.println(n);
+            decrement(--n);
+        }
+    }
