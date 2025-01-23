4958e3051e10d8c608541ec14f0848c0d36fa4a6
Titoputraaa <titobamulo13@gmail.com>
2025-01-23 21:05:22 +0700

    binary seach method

--- Search.java
+++ Search.java
@@ -1,8 +1,9 @@
 public class Search {
     static int[] arr = {1,2,3,4,5,6,7,8,9,};
-    static int target = 5;
+    static int target = 11;
     public static void main(String[] args) {
-        linearSearch();
+        // linearSearch();
+        binarySearch();
     }
 
     static void linearSearch() {
@@ -18,4 +19,27 @@ public class Search {
             System.out.println("target not found");
         }
     }
+
+    static void binarySearch() {
+        boolean find = false;
+        int left = 0;
+        int right = arr.length-1;
+        System.out.println("target is : " + target);
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == arr[mid]) {
+                System.out.println("target in index : " + mid);
+                find = true;
+            }
+            if (target < arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (!find) {
+            System.out.println("target not found");
+        }
+    }
+    
 }
