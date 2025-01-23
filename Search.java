68f78db913d0daf09c8902c240b0df51d85e1bc4
Titoputraaa <titobamulo13@gmail.com>
2025-01-23 21:14:44 +0700

    make menu

--- Search.java
+++ Search.java
@@ -1,12 +1,36 @@
+import java.util.Scanner;
 public class Search {
+    static Scanner sc = new Scanner(System.in);
     static int[] arr = {1,2,3,4,5,6,7,8,9,};
     static int target = 11;
     public static void main(String[] args) {
-        // linearSearch();
-        binarySearch();
+        do {
+            System.out.println("\nwelcome to search algoritma");
+            System.out.println("1 : use linear search algoritma");
+            System.out.println("2 : use binary search algoritma");
+            System.out.println("3 : EXIT");
+            System.out.print("menu : ");
+            int pick = sc.nextInt();
+            switch (pick) {
+                case 1:
+                    linearSearch();
+                    break;
+                case 2:
+                    binarySearch();
+                    break;
+                case 3:
+                    return;
+            
+                default:
+                    System.out.println("plis input the right menu ");
+                    break;
+            }
+
+        } while (true);
     }
 
     static void linearSearch() {
+        System.out.println("\nLINEAR SEARCH");
         System.out.println("target is : " + target);
         boolean find = false;
         for (int i = 0; i < arr.length; i++) {
@@ -21,6 +45,7 @@ public class Search {
     }
 
     static void binarySearch() {
+        System.out.println("\nBINARY SEARCH");
         boolean find = false;
         int left = 0;
         int right = arr.length-1;
